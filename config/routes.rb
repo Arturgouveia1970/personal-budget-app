acbcd58fdd42f0f259b9e7cde3e47e24080b6e2c
Arturgouveia1970 <artgouveiag@gmail.com>
2022-09-22 20:56:20 +0100

    changes for authentications

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :payments
-  resources :categories
-  resources :users
+    resources :categories
+    resources :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  unauthenticated do
+  unauthenticated :users  do
     root 'users#index'
   end
 
-  authenticated do
+  authenticated :users do
     root 'categories#index', as: :authenticated_root
+    
   end
 end
