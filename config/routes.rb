6d5915f01ee8f78ddeda07d599e0d6e4f23f9671
Arturgouveia1970 <artgouveiag@gmail.com>
2022-09-22 20:58:47 +0100

    linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :payments
-    resources :categories
-    resources :users
+  resources :categories
+  resources :users
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
