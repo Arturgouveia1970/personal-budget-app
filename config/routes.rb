b86e31c061dc46a46e3d5eca6133aa1248ce996c
Arturgouveia1970 <artgouveiag@gmail.com>
2022-09-29 16:11:49 +0100

    styling changes

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  unauthenticated :users  do
-    root 'users#index'
-  end
+  # unauthenticated :users  do
+  #   root 'users#index'
+  # end
 
-  authenticated :users do
-    root 'categories#index', as: :authenticated_root
+  # authenticated :users do
+    root 'categories#index'
     
-  end
+  # end
 end
